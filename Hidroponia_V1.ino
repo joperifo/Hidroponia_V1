bd49c15a6b09612cbbebab6e789b51053efa0203
joperifo <joperifo@gmail.com>
2020-08-04 17:50:07 +0100

    Update Hidroponia_V1.ino
    Added code for Temperature sensor and TDS sensor

--- Hidroponia_V1.ino
+++ Hidroponia_V1.ino
@@ -1,24 +1,36 @@
-#include <EEPROM.h>
+#include <eeprom.h>
 #include <GravityTDS.h>
 #include <OneWire.h> 
 #include <DallasTemperature.h>
+#include <DHT.h>
 
 #define TdsSensorPin A1
+#define ONE_WIRE_BUS 5
+#define DHT11_PIN 4
 
 GravityTDS SensorTDS;
 
 float temperature = 25,tdsValue = 0;
 
-unsigned long ticks;
-unsigned long last_tick_2000, last_tick_1000 ;
+uint32_t ticks, last_tick_2000, last_tick_1000 ;
+float WaterTemp=0;
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature WaterTempSensor(&oneWire);
 
 void setup() {
 
+  //Init Serial
+  Serial.begin(115200);
   //TDS Sensor
   InitSensorTDS();
-  //Init Serial
-  Serial.begin(115200);  
-
+  //Water Temperature Sensor
+  WaterTempSensor.begin();
+  //DHT11 Test
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT_LIB_VERSION);
+  Serial.println();
+  
 }
 
 void loop() {
@@ -27,19 +39,32 @@ void loop() {
 
   //2000ms timer
   if ((ticks - last_tick_2000) > 2000)
-    {  
-      //temperature = readTemperature();  //add your temperature sensor and read it
-      SensorTDS.setTemperature(temperature);  // set the temperature and execute temperature compensation
+    { 
+      #pragma region Exterior Temp and Humidity
+            
+      #pragma endregion 
+
+      #pragma region Water Temperature Read
+      sensors.requestTemperatures(); 
+      WaterTemp=sensors.getTempCByIndex(0);      
+      Serial.print(" Water Temperature= ");
+      Serial.print(Celcius);
+      #pragma endregion
+      
+      #pragma region Water TDS Read
+      SensorTDS.setTemperature(WaterTemp);  // set the temperature and execute temperature compensation
       SensorTDS.update();  //sample and calculate 
       tdsValue = SensorTDS.getTdsValue();  // then get the value
       Serial.print(tdsValue,0);
-      Serial.println("ppm");      
+      Serial.println("ppm");
+      #pragma endregion  
   
       last_tick_2000 = ticks;
     } 
 
 }
 
+#pragma region InitSensorTDS
 void InitSensorTDS()
 {
   SensorTDS.setPin(TdsSensorPin);
@@ -47,3 +72,4 @@ void InitSensorTDS()
   SensorTDS.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
   SensorTDS.begin();  //initialization
 }
+#pragma endregion
