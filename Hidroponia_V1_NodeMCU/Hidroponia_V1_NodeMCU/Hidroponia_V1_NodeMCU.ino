60575210fa27039735034357f892a76210630a73
joperifo <joperifo@gmail.com>
2020-11-15 18:05:10 +0000

    MQTT code added to Node MCU

--- Hidroponia_V1_NodeMCU/Hidroponia_V1_NodeMCU/Hidroponia_V1_NodeMCU.ino
+++ Hidroponia_V1_NodeMCU/Hidroponia_V1_NodeMCU/Hidroponia_V1_NodeMCU.ino
@@ -5,10 +5,41 @@
 #include <Adafruit_SSD1306.h>
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
-
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #define ON 1
 #define OFF 0
+
+#pragma region MQTT vars
+
+// WiFi
+const char* ssid = "JFMR@HOME";                 // Your personal network SSID
+const char* wifi_password = "Misty@2019"; // Your personal network password
+
+// MQTT
+bool MqttEnable = false;
+int WIFI_retries = 0;
+
+const char* mqtt_server = "192.168.0.123";  // IP of the MQTT broker
+const char* Exthumidity_topic_in = "Hidroponics/1/Sensors/Humidity";
+const char* Exttemperature_topic_in = "Hidroponics/1/Sensors/Temperature";
+const char* Watertemperature_topic_in = "Hidroponics/1/Sensors/WaterTemperature";
+const char* pH_topic_in = "Hidroponics/1/Sensors/pH";
+const char* TDS_topic_in = "Hidroponics/1/Sensors/TDS";
+const char* nutrients_topic_out = "Hidroponics/1/Actuators/nutrients_pump";
+const char* feed_topic_out = "Hidroponics/1/Actuators/feed_pump";
+const char* pH_topic_out = "Hidroponics/1/Actuators/pH_pump";
+
+const char* mqtt_username = "admin"; // MQTT username
+const char* mqtt_password = "admin"; // MQTT password
+const char* clientID = "NodeMCU-Hidro-1"; // MQTT client ID
+
+// Initialise the WiFi and MQTT Client objects
+WiFiClient wifiClient;
+// 1883 is the listener port for the Broker
+PubSubClient client(mqtt_server, 1883, wifiClient); 
+#pragma endregion
 
 //Pins Define
    
@@ -30,8 +61,6 @@ SoftwareSerial s(RxPin,TxPin); //Rx D6 Tx D5
 DeserializationError error;
 
 //Global define
-
-
 Fuzzy *fuzzy = new Fuzzy();
 
 
@@ -57,6 +86,7 @@ uint32_t ticks, last_tick_20ms, last_tick_500ms, last_tick_1000ms,timmer_1s,timm
 
 #pragma region Init Setup
 void setup() {
+
   FirstCycle=1;
   pinMode(RxPin, INPUT);
   pinMode(TxPin, OUTPUT);
@@ -106,6 +136,47 @@ void setup() {
   display.display();
 
   delay(1500);
+  #pragma endregion 
+
+  #pragma region Init Mqtt 
+  if(MqttEnable)
+  {
+    bool mqtt_connected = false;
+
+    WIFI_retries = 0;
+
+    mqtt_connected = connect_MQTT();
+
+    if(mqtt_connected)
+    {
+      display.clearDisplay();
+      display.setCursor(20,5);
+      display.println("MQTT");
+      display.setCursor(0,15);
+      display.println("Connected to:");
+      display.setCursor(50,30);
+      display.println(mqtt_server);
+      display.setCursor(0,45);
+      display.println("=====================");
+      display.display();
+    }
+    else
+    {
+      display.clearDisplay();
+      display.setCursor(20,5);
+      display.println("MQTT");
+      display.setCursor(0,15);
+      display.println("=====================");
+      display.setCursor(50,30);
+      display.println("NOK");
+      display.setCursor(0,45);
+      display.println("=====================");
+      display.display();
+
+      for(;;);
+    }
+  }
+    
   #pragma endregion
 
   #pragma region Print splash screen
@@ -121,13 +192,13 @@ void setup() {
   display.setCursor(0,45);
   display.println("=====================");
   display.display();
-  delay(1500);
-  #pragma endregion
-  
+  delay(1500);  
   // Set a random seed, just for test purposes
   randomSeed(analogRead(0)); 
 
-#pragma region Init Fuzzy Logic pH pump
+  #pragma endregion
+
+  #pragma region Init Fuzzy Logic pH pump
   //Init fuzzy logi controller for pH
   FuzzyOK_pH = InitFuzzyPh();  
 
@@ -140,18 +211,14 @@ void setup() {
   display.println("OK");
   display.setCursor(0,45);
   display.println("=====================");
-  display.display();
+  display.display();  
+}
   #pragma endregion
 
-  
-}
 #pragma endregion
 
 void loop() {
   // just for testing proposes
- 
- 
-
   ticks = millis();
 
       #pragma region First cycle
@@ -342,6 +409,83 @@ void loop() {
 
       if(timmer_10m>600)
       {
+    
+        #pragma region Send MQTT values
+
+        if(MqttEnable)
+        {
+
+          #pragma region Construct MQTT strings
+          // MQTT construct strings to send
+          /*String HumExtSensor="ExtHum: "+String((float)ExtHum)+" % ";
+          String TempExtSensor="ExtTemp: "+String((float)ExtTemp)+" C ";
+          String TempWaterSensor="WaterTemp: "+String((float)WaterTemp)+" C ";
+          String pHWaterSensor="WaterPH: "+String((float)Ph_Value);
+          String TdsWaterSensor="WaterTDS: "+String((float)Tds_Value)+" ppm ";*/
+          #pragma endregion
+
+          #pragma region Send External humidity to MQTT
+          if (client.publish(Exthumidity_topic_in, String(ExtHum).c_str())) {
+            Serial.println("External Humidity sent!");
+          }
+          else {
+            Serial.println("External Humidity failed to send. Reconnecting to MQTT Broker and trying again");
+            client.connect(clientID, mqtt_username, mqtt_password);
+            delay(10); 
+            client.publish(Exthumidity_topic_in, String(ExtHum).c_str());
+          }
+          #pragma endregion
+
+          #pragma region Send External temperature to MQTT
+          if (client.publish(Exttemperature_topic_in, String(ExtTemp).c_str())) {
+            Serial.println("External Temperature sent!");
+          }
+          else {
+            Serial.println("External Temperature failed to send. Reconnecting to MQTT Broker and trying again");
+            client.connect(clientID, mqtt_username, mqtt_password);
+            delay(10); 
+            client.publish(Exttemperature_topic_in, String(ExtTemp).c_str());
+          }
+          #pragma endregion
+
+          #pragma region Send Water temperature to MQTT
+          if (client.publish(Watertemperature_topic_in, String(WaterTemp).c_str())) {
+            Serial.println("Water Temperature sent!");
+          }
+          else {
+            Serial.println("Water Temperature failed to send. Reconnecting to MQTT Broker and trying again");
+            client.connect(clientID, mqtt_username, mqtt_password);
+            delay(10); 
+            client.publish(Watertemperature_topic_in, String(WaterTemp).c_str());
+          }
+          #pragma endregion
+
+          #pragma region Send Water pH to MQTT
+          if (client.publish(pH_topic_in, String(Ph_Value).c_str())) {
+            Serial.println("Water pH sent!");
+          }
+          else {
+            Serial.println("Water pH failed to send. Reconnecting to MQTT Broker and trying again");
+            client.connect(clientID, mqtt_username, mqtt_password);
+            delay(10); 
+            client.publish(pH_topic_in, String(Ph_Value).c_str());
+          }
+          #pragma endregion
+
+          #pragma region Send Water Tds to MQTT
+          if (client.publish(TDS_topic_in, String(Tds_Value).c_str())) {
+            Serial.println("Water Tds sent!");
+          }
+          else {
+            Serial.println("Water Tds failed to send. Reconnecting to MQTT Broker and trying again");
+            client.connect(clientID, mqtt_username, mqtt_password);
+            delay(10); 
+            client.publish(TDS_topic_in, String(Tds_Value).c_str());
+          }
+          #pragma endregion
+        
+        }
+        #pragma endregion
 
         timmer_10m=0;
       }
@@ -380,91 +524,132 @@ void loop() {
 
 #pragma region Aux functions
 
+ #pragma region MQTT Aux Functions
+
+  bool connect_MQTT(){
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    // Connect to the WiFi
+    WiFi.begin(ssid, wifi_password);
+
+    // Wait until the connection has been confirmed before continuing
+    while (WiFi.status() != WL_CONNECTED && WIFI_retries < 10)  {
+      delay(500);
+      Serial.print(".");
+      WIFI_retries++;
+    }
+    if(WIFI_retries >= 10)
+    {
+      return false;
+    }
+
+    // Debugging - Output the IP Address of the ESP8266
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Connect to MQTT Broker
+    // client.connect returns a boolean value to let us know if the connection was successful.
+    // If the connection is failing, make sure you are using the correct MQTT Username and Password (Setup Earlier in the Instructable)
+    if (client.connect(clientID, mqtt_username, mqtt_password)) {
+      Serial.println("Connected to MQTT Broker!");
+    }
+    else {
+      Serial.println("Connection to MQTT Broker failed...");
+    }
+
+    return true;
+}
+ 
+ #pragma endregion
 
 #pragma endregion
 
 #pragma region FuzzyLogic
 
-bool InitFuzzyPh()
-{
-  // Instantiating a FuzzyInput object
-  FuzzyInput *ph = new FuzzyInput(1);
-  // Instantiating a FuzzySet object
-  FuzzySet *acid = new FuzzySet(6.0, 6.5, 7.0, 7.5);
-  // Including the FuzzySet into FuzzyInput
-  ph->addFuzzySet(acid);
-  // Instantiating a FuzzySet object
-  FuzzySet *neutral = new FuzzySet(7.0, 7.5, 8.0, 8.5);
-  // Including the FuzzySet into FuzzyInput
-  ph->addFuzzySet(neutral);
-  // Instantiating a FuzzySet object
-  FuzzySet *base = new FuzzySet(8.0, 8.5, 10.0, 14.0);
-  // Including the FuzzySet into FuzzyInput
-  ph->addFuzzySet(base);
-  // Including the FuzzyInput into Fuzzy
-  fuzzy->addFuzzyInput(ph);
+#pragma region pH pump
+  bool InitFuzzyPh()
+  {
+    // Instantiating a FuzzyInput object
+    FuzzyInput *ph = new FuzzyInput(1);
+    // Instantiating a FuzzySet object
+    FuzzySet *acid = new FuzzySet(6.0, 6.5, 7.0, 7.5);
+    // Including the FuzzySet into FuzzyInput
+    ph->addFuzzySet(acid);
+    // Instantiating a FuzzySet object
+    FuzzySet *neutral = new FuzzySet(7.0, 7.5, 8.0, 8.5);
+    // Including the FuzzySet into FuzzyInput
+    ph->addFuzzySet(neutral);
+    // Instantiating a FuzzySet object
+    FuzzySet *base = new FuzzySet(8.0, 8.5, 10.0, 14.0);
+    // Including the FuzzySet into FuzzyInput
+    ph->addFuzzySet(base);
+    // Including the FuzzyInput into Fuzzy
+    fuzzy->addFuzzyInput(ph);
 
-  // Instantiating a FuzzyOutput objects
-  FuzzyOutput *phPumpPulse = new FuzzyOutput(1);
-  // Instantiating a FuzzySet object
-  FuzzySet *small = new FuzzySet(50,200, 200, 300);
-  // Including the FuzzySet into FuzzyOutput
-  phPumpPulse->addFuzzySet(small);
-  // Instantiating a FuzzySet object
-  FuzzySet *medium = new FuzzySet(200, 400, 400, 500);
-  // Including the FuzzySet into FuzzyOutput
-  phPumpPulse->addFuzzySet(medium);
-  // Instantiating a FuzzySet object
-  FuzzySet *big = new FuzzySet(400, 500, 700, 800);
-  // Including the FuzzySet into FuzzyOutput
-  phPumpPulse->addFuzzySet(big);
-  // Including the FuzzyOutput into Fuzzy
-  fuzzy->addFuzzyOutput(phPumpPulse);
-  
-  // Building FuzzyRule "IF ph = acid THEN phPumpPulse = small"
-  // Instantiating a FuzzyRuleAntecedent objects
-  FuzzyRuleAntecedent *ifPhAcid = new FuzzyRuleAntecedent();
-  // Creating a FuzzyRuleAntecedent with just a single FuzzySet
-  ifPhAcid->joinSingle(acid);
-  // Instantiating a FuzzyRuleConsequent objects
-  FuzzyRuleConsequent *thenphPumpPulseSmall = new FuzzyRuleConsequent();
-  // Including a FuzzySet to this FuzzyRuleConsequent
-  thenphPumpPulseSmall->addOutput(small);
-  // Instantiating a FuzzyRule objects
-  FuzzyRule *fuzzyRule01 = new FuzzyRule(1, ifPhAcid, thenphPumpPulseSmall);
-  // Including the FuzzyRule into Fuzzy
-  fuzzy->addFuzzyRule(fuzzyRule01);
+    // Instantiating a FuzzyOutput objects
+    FuzzyOutput *phPumpPulse = new FuzzyOutput(1);
+    // Instantiating a FuzzySet object
+    FuzzySet *small = new FuzzySet(500,2000, 2000, 3000);
+    // Including the FuzzySet into FuzzyOutput
+    phPumpPulse->addFuzzySet(small);
+    // Instantiating a FuzzySet object
+    FuzzySet *medium = new FuzzySet(2000, 4000, 4000, 5000);
+    // Including the FuzzySet into FuzzyOutput
+    phPumpPulse->addFuzzySet(medium);
+    // Instantiating a FuzzySet object
+    FuzzySet *big = new FuzzySet(4000, 5000, 7000, 8000);
+    // Including the FuzzySet into FuzzyOutput
+    phPumpPulse->addFuzzySet(big);
+    // Including the FuzzyOutput into Fuzzy
+    fuzzy->addFuzzyOutput(phPumpPulse);
+    
+    // Building FuzzyRule "IF ph = acid THEN phPumpPulse = small"
+    // Instantiating a FuzzyRuleAntecedent objects
+    FuzzyRuleAntecedent *ifPhAcid = new FuzzyRuleAntecedent();
+    // Creating a FuzzyRuleAntecedent with just a single FuzzySet
+    ifPhAcid->joinSingle(acid);
+    // Instantiating a FuzzyRuleConsequent objects
+    FuzzyRuleConsequent *thenphPumpPulseSmall = new FuzzyRuleConsequent();
+    // Including a FuzzySet to this FuzzyRuleConsequent
+    thenphPumpPulseSmall->addOutput(small);
+    // Instantiating a FuzzyRule objects
+    FuzzyRule *fuzzyRule01 = new FuzzyRule(1, ifPhAcid, thenphPumpPulseSmall);
+    // Including the FuzzyRule into Fuzzy
+    fuzzy->addFuzzyRule(fuzzyRule01);
 
-  // Building FuzzyRule "IF ph = neutral THEN phPumpPulse = medium"
-  // Instantiating a FuzzyRuleAntecedent objects
-  FuzzyRuleAntecedent *ifPhNeutral = new FuzzyRuleAntecedent();
-  // Creating a FuzzyRuleAntecedent with just a single FuzzySet
-  ifPhNeutral->joinSingle(neutral);
-  // Instantiating a FuzzyRuleConsequent objects
-  FuzzyRuleConsequent *thenphPumpPulseMedium = new FuzzyRuleConsequent();
-  // Including a FuzzySet to this FuzzyRuleConsequent
-  thenphPumpPulseMedium->addOutput(medium);
-  // Instantiating a FuzzyRule objects
-  FuzzyRule *fuzzyRule02 = new FuzzyRule(2, ifPhNeutral, thenphPumpPulseMedium);
-  // Including the FuzzyRule into Fuzzy
-  fuzzy->addFuzzyRule(fuzzyRule02);
+    // Building FuzzyRule "IF ph = neutral THEN phPumpPulse = medium"
+    // Instantiating a FuzzyRuleAntecedent objects
+    FuzzyRuleAntecedent *ifPhNeutral = new FuzzyRuleAntecedent();
+    // Creating a FuzzyRuleAntecedent with just a single FuzzySet
+    ifPhNeutral->joinSingle(neutral);
+    // Instantiating a FuzzyRuleConsequent objects
+    FuzzyRuleConsequent *thenphPumpPulseMedium = new FuzzyRuleConsequent();
+    // Including a FuzzySet to this FuzzyRuleConsequent
+    thenphPumpPulseMedium->addOutput(medium);
+    // Instantiating a FuzzyRule objects
+    FuzzyRule *fuzzyRule02 = new FuzzyRule(2, ifPhNeutral, thenphPumpPulseMedium);
+    // Including the FuzzyRule into Fuzzy
+    fuzzy->addFuzzyRule(fuzzyRule02);
 
-  // Building FuzzyRule "IF ph = base THEN phPumpPulse = big"
-  // Instantiating a FuzzyRuleAntecedent objects
-  FuzzyRuleAntecedent *ifPhBase = new FuzzyRuleAntecedent();
-  // Creating a FuzzyRuleAntecedent with just a single FuzzySet
-  ifPhBase->joinSingle(base);
-  // Instantiating a FuzzyRuleConsequent objects
-  FuzzyRuleConsequent *thenphPumpPulseBig = new FuzzyRuleConsequent();
-  // Including a FuzzySet to this FuzzyRuleConsequent
-  thenphPumpPulseBig->addOutput(big);
-  // Instantiating a FuzzyRule objects
-  FuzzyRule *fuzzyRule03 = new FuzzyRule(3, ifPhBase, thenphPumpPulseBig);
-  // Including the FuzzyRule into Fuzzy
-  fuzzy->addFuzzyRule(fuzzyRule03);  
+    // Building FuzzyRule "IF ph = base THEN phPumpPulse = big"
+    // Instantiating a FuzzyRuleAntecedent objects
+    FuzzyRuleAntecedent *ifPhBase = new FuzzyRuleAntecedent();
+    // Creating a FuzzyRuleAntecedent with just a single FuzzySet
+    ifPhBase->joinSingle(base);
+    // Instantiating a FuzzyRuleConsequent objects
+    FuzzyRuleConsequent *thenphPumpPulseBig = new FuzzyRuleConsequent();
+    // Including a FuzzySet to this FuzzyRuleConsequent
+    thenphPumpPulseBig->addOutput(big);
+    // Instantiating a FuzzyRule objects
+    FuzzyRule *fuzzyRule03 = new FuzzyRule(3, ifPhBase, thenphPumpPulseBig);
+    // Including the FuzzyRule into Fuzzy
+    fuzzy->addFuzzyRule(fuzzyRule03);  
 
-  return true;
+    return true;
 }
+#pragma endregion
 
 #pragma endregion
 
